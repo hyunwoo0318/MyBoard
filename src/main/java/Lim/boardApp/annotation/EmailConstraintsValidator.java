68caef94fa96ae670d15f0a36597c8db4619bc4a
hyunwoo0318 <hyunwoo0318@naver.com>
2023-08-13 10:35:32 +0900

    baseService 추가

--- src/main/java/Lim/boardApp/annotation/EmailConstraintsValidator.java
+++ src/main/java/Lim/boardApp/annotation/EmailConstraintsValidator.java
@@ -5,7 +5,6 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Pattern;
 
 public class EmailConstraintsValidator implements ConstraintValidator<CustomEmail, String> {
-
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if(value==null) return false;
